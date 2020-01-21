d6a945041b8d4d74c253184bd548d79d5282f415
crotsu <crotsu@gmail.com>
2020-01-21 20:41:24 +0900

    chap4 リネーム

--- chap4_Application/Preprocessing/1_rename.py
+++ chap4_Application/Preprocessing/1_rename.py
@@ -1,1 +1,39 @@
+import glob
 import os
+
+#
+# 災害画像と非災害画像の名前を変更する
+#
+
+# ディレクトリがdisasterとnon_disasterなので，これをループするようにする
+prefix_name = ["", "non_"]
+
+for pre in prefix_name:
+    path = "../disaster_decision/" + pre + "disaster/*"
+
+    # ファイル名を取得(lsと同じ)
+    files = glob.glob(path)
+    #print(files)
+
+    basename = pre + "disasterIMG_"
+
+    ct = 0
+    for filename in files:
+
+        print(filename)
+    
+        # ファイル名と拡張子に分ける
+        root, ext = os.path.splitext(filename)
+        
+        # 拡張子をすべて小文字にする（例：JPG -> jpg）
+        ext = ext.lower()
+        
+        # リネームする
+        new_filename = path[:-1] + basename + str(ct) + ext
+        #print(new_filename)
+        os.rename(filename, new_filename)
+        
+        ct += 1
+
+    print("")
+    print("[%s] is finished!"%(pre))
