3ea2d6ed35738606470ac4e7ba56b34bb1c45a8d
crotsu <crotsu@gmail.com>
2020-01-25 15:19:36 +0900

    chap2_1を修正

---
+++ chap2_Python/program/chap2_1_1.py
@@ -0,0 +1,5 @@
+# 問1
+total = 0
+for i in range(1,10+1):
+    total += i
+print("total=", total)
---
+++ chap2_Python/program/chap2_1_2.py
@@ -0,0 +1,7 @@
+# 問2
+total = 0
+for i in range(1,100+1):
+    if i%3==0:
+        total += i
+        
+print("total=", total)
