aa59faa5c32bc076ceb54b2621bed38922daa290
crotsu <crotsu@gmail.com>
2020-01-25 13:38:40 +0900

    chap1を修正

--- chap1_colab/program/mnist_fit.py
+++ chap1_colab/program/mnist_fit.py
@@ -38,18 +38,3 @@ model.evaluate(x_test,  y_test, verbose=2)
 # 学習済みのモデルを保存する
 # 重みの保存
 model.save_weights('mnist_weight')
-
-
-# MNISTのテスト画像を表示する
-# mnistの画像を表示する
-import matplotlib.pyplot as plt
-
-W = 4  # 横に並べる個数
-H = 3  # 縦に並べる個数
-fig = plt.figure(figsize=(H, W))
-fig.subplots_adjust(left=0, right=1, bottom=0, top=1.0, hspace=0.05, wspace=0.05)
-for i in range(W*H):
-    ax = fig.add_subplot(H, W, i + 1, xticks=[], yticks=[])
-    ax.imshow(x_test[i].reshape((28, 28)), cmap='gray')
-
-plt.show()
